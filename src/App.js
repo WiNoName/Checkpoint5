a2076159c832aa0ce00c71c2224613866cc5b2d5
NoNameAB <48261117+NoNameAB@users.noreply.github.com>
2019-06-26 15:17:17 +0100

    Checkpoint

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import './style.css';
+import Profile from './Components/Profile.js';
+
+function App() {
+ return(
+     <Profile/>
+ )
+}
+
+export default App;
